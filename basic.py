b994cd353ee84e2e213b42aba8195b6a9898cbb2
L.Yihe <anish_alpha@hotmail.com>
2020-11-01 16:02:05 +0000

    rename file

--- basic.py
+++ basic.py
@@ -7,23 +7,25 @@ Created on Sun Nov  1 13:07:52 2020
 import numpy as np
 import matplotlib.pyplot as plt
 # init params
-item_num = 5
+item_num = 8
 M = np.zeros((item_num, item_num))
 for i in range(item_num - 1):
     M[i, i] = 1
     M[i, i + 1] = 1
 
 # init experiment
-X = np.asarray([0, 1, 0, 1, 1])
+inputs = [4, 5]
+X = np.zeros(item_num)
+X[inputs] = 1
 Y = X.astype(np.float)
-output = np.empty((0, item_num))
+outputs = np.empty((0, item_num))
 epochs = range(100)
 for t in epochs:
     dY = -np.dot(M, np.dot(M.T, Y) - X) * 0.1
     Y += dY
-    output = np.append(output, [Y], axis = 0)
+    outputs = np.append(outputs, [Y], axis = 0)
 
 fig, ax = plt.subplots()
 for i in range(item_num):
-    ax.plot(epochs, output[:, i], label = i)
+    ax.plot(epochs, outputs[:, i], label = i)
 ax.legend()
