2c1ebaa60755acc2fccc6c035c585308c841bf3b
L.Yihe <anish_alpha@hotmail.com>
2020-11-01 15:20:24 +0000

    polish plots for episodic recall; generalisation not working

--- test.py
+++ test.py
@@ -5,18 +5,25 @@ Created on Sun Nov  1 13:07:52 2020
 @author: anish
 """
 import numpy as np
+import matplotlib.pyplot as plt
+# init params
+item_num = 5
+M = np.zeros((item_num, item_num))
+for i in range(item_num - 1):
+    M[i, i] = 1
+    M[i, i + 1] = 1
 
-M = np.asarray([[1, 1, 0, 0, 0],
-                [0, 1, 1, 0, 0],
-                [0, 0, 1, 1, 0],
-                [0, 0, 0, 1, 1],
-                [0, 0, 0, 0, 0]])
-
-Y = np.zeros(5)
-Z = Y
-X = np.asarray([0, 0, 1, 1, 0])
-
-for t in range(100):
+# init experiment
+X = np.asarray([0, 1, 0, 1, 1])
+Y = X.astype(np.float)
+output = np.empty((0, item_num))
+epochs = range(100)
+for t in epochs:
     dY = -np.dot(M, np.dot(M.T, Y) - X) * 0.1
     Y += dY
-    print(Y)
+    output = np.append(output, [Y], axis = 0)
+
+fig, ax = plt.subplots()
+for i in range(item_num):
+    ax.plot(epochs, output[:, i], label = i)
+ax.legend()
