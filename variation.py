b59c471d05428a2009625395fe31f14da2fd8e70
L.Yihe <anish_alpha@hotmail.com>
2020-11-01 23:29:30 +0000

    generalisation working with hack

--- variation.py
+++ variation.py
@@ -8,33 +8,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from general_funs import *
 # init params
-item_num = 8
+item_num = 9
 M = np.zeros((item_num, item_num))
 for i in range(item_num - 1):
-    M[i, i] = 1
-    M[i, i + 1] = 1
-
+    # M[i, i] += 0.5
+    M[i, i + 1] += 1
+    M[i + 1, i] -= 1
+M[0, 0] = 1
+M[-1, -1] = -1
 # init experiment
-inputs = [2, 3]
+inputs = [0, 8]
 X = np.zeros(item_num)
-X[inputs] = .5
 Y = X.astype(np.float)
+Z = Y
+X[inputs] = 1
 outputs = np.empty((0, item_num))
+ys = outputs
 epochs = range(1000)
 for t in epochs:
     dY = -np.dot(M, np.dot(M.T, Y) - X) * 0.1
-    Y += dY + np.random.randn(item_num) / 100
-    # Z = np.multiply(softmax(Y), X)
-    # Z = Z / np.sum(Z)
-    Z = softmax(Y)
-    dZ = Z - Y
-    # print(dZ)
-    Y -= dY
-    outputs = np.append(outputs, [Y], axis = 0)
+    Y += dY #+ np.random.randn(item_num) / 1000 
+    ys = np.append(ys, [Y], axis = 0)
+outputs = softmax(ys[:, inputs])
 
-fig, ax = plt.subplots()
-for i in range(item_num):
-    ax.plot(epochs, outputs[:, i], label = i)
-ax.legend()
+fig, ax = plt.subplots(2, 1)
+for i in inputs:
+    ax[0].plot(epochs, ys[:, i], label = 'Y[{}]'.format(i))
+for i in range(2):
+    ax[1].plot(epochs, outputs[:, i], label = 'output[{}]'.format(i))
+ax[0].legend(loc = 'upper right')
 
 
