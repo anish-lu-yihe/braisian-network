028e2c1e6604efe7ee5d59963c9aca79b3be46d2
L.Yihe <anish_alpha@hotmail.com>
2020-11-11 22:09:37 +0000

    two models

--- basic.py
+++ basic.py
@@ -7,25 +7,36 @@ Created on Sun Nov  1 13:07:52 2020
 import numpy as np
 import matplotlib.pyplot as plt
 # init params
-item_num = 8
-M = np.zeros((item_num, item_num))
-for i in range(item_num - 1):
+x_num, y_num = 8, 7
+M = np.zeros((y_num, x_num))
+for i in range(y_num):
     M[i, i] = 1
     M[i, i + 1] = 1
 
 # init experiment
-inputs = [4, 5]
-X = np.zeros(item_num)
-Y = X.astype(np.float)
-X[inputs] = 1
-outputs = np.empty((0, item_num))
+inputs = [2,4]
+epsilon = 0.01
+rate = 0.1
+X1, X2 = np.zeros(x_num), np.full(x_num, epsilon)
+Y1, Y2 = np.zeros(y_num), np.full(y_num, epsilon)
+X1[inputs], X2[inputs] = 1, 1
+outputs1, outputs2 = np.empty((0, y_num)), np.empty((0, y_num))
 epochs = range(100)
 for t in epochs:
-    dY = -np.dot(M, np.dot(M.T, Y) - X) * 0.1
-    Y += dY
-    outputs = np.append(outputs, [Y], axis = 0)
+    dY1 = -np.dot(M, np.dot(M.T, Y1) - X1) + np.abs(np.random.normal(0, epsilon, y_num))
+    Y1 += dY1 * rate
+    outputs1 = np.append(outputs1, [Y1], axis = 0)
+    
+    E = np.divide(X2, np.dot(M.T, Y2))
+    bracket = np.divide(np.dot(M, E), np.sum(M, axis = 1)) - 1
+    dY2 = np.multiply(Y2, bracket) + np.abs(np.random.normal(0, epsilon, y_num))
+    Y2 += dY2 * rate
+    outputs2 = np.append(outputs2, [Y2], axis = 0)
 
-fig, ax = plt.subplots()
-for i in range(item_num):
-    ax.plot(epochs, outputs[:, i], label = i)
-ax.legend()
+fig, ax = plt.subplots(1, 2)
+for i in range(y_num):
+    ax[0].plot(epochs, outputs1[:, i], label = i)
+    ax[1].plot(epochs, outputs2[:, i], label = i)
+ax[0].legend()
+ax[1].legend()
+
