5d2a98095db01df4c22b3ed22bd156c53f16b326
L.Yihe <anish_alpha@hotmail.com>
2020-11-14 17:48:56 +0000

    add class feedback_inhibition_network

--- basic.py
+++ basic.py
@@ -10,9 +10,8 @@ import matplotlib.pyplot as plt
 x_num, y_num = 8, 7
 M = np.zeros((y_num, x_num))
 for i in range(y_num):
-    M[i, i] = 1
-    M[i, i + 1] = 1
-
+    M[i, i:i+2] = 1
+# M[-1]=M[1]
 # init experiment
 inputs = [2,4]
 epsilon = 0.01
@@ -20,6 +19,9 @@ rate = 0.1
 X1, X2 = np.zeros(x_num), np.full(x_num, epsilon)
 Y1, Y2 = np.zeros(y_num), np.full(y_num, epsilon)
 X1[inputs], X2[inputs] = 1, 1
+# X1 = [0,0,0.1,0.5,0.5,0.7,0,0]
+# X2 = [epsilon,epsilon,0.1,0.5,0.5,0.7,epsilon,epsilon]
+print(M), print(X1)
 outputs1, outputs2 = np.empty((0, y_num)), np.empty((0, y_num))
 epochs = range(100)
 for t in epochs:
@@ -27,7 +29,9 @@ for t in epochs:
     Y1 += dY1 * rate
     outputs1 = np.append(outputs1, [Y1], axis = 0)
     
-    E = np.divide(X2, np.dot(M.T, Y2))
+    xxx = np.dot(M.T, Y2)
+    xxx[xxx == 0] = epsilon
+    E = np.divide(X2, xxx)
     bracket = np.divide(np.dot(M, E), np.sum(M, axis = 1)) - 1
     dY2 = np.multiply(Y2, bracket) + np.abs(np.random.normal(0, epsilon, y_num))
     Y2 += dY2 * rate
@@ -38,5 +42,5 @@ for i in range(y_num):
     ax[0].plot(epochs, outputs1[:, i], label = i)
     ax[1].plot(epochs, outputs2[:, i], label = i)
 ax[0].legend()
-ax[1].legend()
+# ax[1].legend()
 
