ade401fa6525f5cbdf755efb667321884f8dc1c1
L.Yihe <anish_alpha@hotmail.com>
2020-11-17 18:16:58 +0000

    refine fin

--- basic.py
+++ basic.py
@@ -8,26 +8,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from funs import *
 # init params
-x_num, y_num = 10, 5
+x_num, y_num = 100, 100
 item_num = 3
 X = np.random.randint(2, size = (item_num, x_num))
 Y = np.random.randint(2, size = (item_num, y_num))
-Xfin, Xbin = feedback_inhibition_network('d'), feedback_inhibition_network('d')
-Yfin, Ybin = feedback_inhibition_network('d'), feedback_inhibition_network('d')
-Xfin.setup_matrix(X), Xbin.setup_matrix(X.T)
-Yfin.setup_matrix(Y), Ybin.setup_matrix(Y.T)
-key1 = Xfin.recognise(X[0])[-1]
-outputs1 = np.dot(Y.T, key1)
-key2 = Yfin.recognise(Y[0])[-1]
-outputs2 = np.dot(X.T, key2)
+Xfin, Yfin = feedback_inhibition_network('d'), feedback_inhibition_network('d')
+Xfin.setup_matrix(X), Yfin.setup_matrix(Y)
 
-print(key1, outputs1, Y[0])
-print(key2, outputs2, X[0])
+epoch = 1000
+key1 = Xfin.recognise(X[0], epoch)
+outputs1 = np.dot(key1, Y)
+key2 = Yfin.recognise(Y[0], epoch)
+outputs2 = np.dot(key2, X)
 
-# fig, ax = plt.subplots(1, 2)
-# for i in range(y_num):
-#     ax[0].plot(epochs, outputs1[:, i], label = i)
-#     ax[1].plot(epochs, outputs2[:, i], label = i)
-# ax[0].legend()
-# # ax[1].legend()
+diff1x = np.max(np.abs(key1 - [1,0,0]), axis = 1)
+diff2y = np.max(np.abs(key2 - [1,0,0]), axis = 1)
+diff1y = np.max(np.abs(outputs1 - Y[0]), axis = 1)
+diff2x = np.max(np.abs(outputs2 - X[0]), axis = 1)
+
+epochs = range(epoch + 1)
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(epochs[1:], diff1x[1:])
+ax[1].plot(epochs[1:], diff2y[1:])
+ax[0].plot(epochs[1:], diff1y[1:])
+ax[1].plot(epochs[1:], diff2x[1:])
+ax[0].set_xscale('log')
+ax[1].set_xscale('log')
 
--- funs.py
+++ funs.py
@@ -17,10 +17,12 @@ class feedback_inhibition_network:
         self.M = np.asarray(M)
         self.num_Y, self.num_X = self.M.shape
 
-    def recognise(self, X, epoch = 1000, rate = 0.1, epsilon = 0.01):
+    def recognise(self, X, epoch = 1000, rate = 0.1, epsilon = 0.001):
         recog_methods = {'divisive': self.__recognise_div,
                          'subtractive': self.__recognise_sub}
         return recog_methods[self.inhitype](X, epoch, rate, epsilon)
+        # else:
+        #     return np.asarray([np.dot(self.M, X)])
         
     def __recognise_sub(self, X, epoch, rate, epsilon):
         Y = np.zeros(self.num_Y)
@@ -35,13 +37,13 @@ class feedback_inhibition_network:
     
     def __recognise_div(self, X, epoch, rate, epsilon):
         Y = np.zeros(self.num_Y)
-        V = np.sum(self.M, axis = 1)
-        outputs = np.empty((0, self.num_Y))
+        V = np.sum(self.M, axis = 1).astype(np.float)
+        V[V == 0] = epsilon
+        outputs = [np.dot(self.M, X)]
         for t in range(epoch):
-            Xe = np.dot(self.M.T, Y) 
+            Xe = np.dot(self.M.T, Y).astype(np.float)
             Xe[Xe == 0] = epsilon
             E = np.divide(X, Xe)
-            V[V == 0] = epsilon
             dY = np.multiply(Y, np.divide(np.dot(self.M, E), V) - 1)
             noise = np.abs(np.random.normal(scale = epsilon, size = self.num_Y))
             Y += dY * rate + noise
