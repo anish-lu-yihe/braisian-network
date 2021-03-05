0ca500ee3c4e59933899babda2fc42a78f1b2210
L.Yihe <anish_alpha@hotmail.com>
2021-03-05 15:50:27 +0000

    clean code

--- ifn.py
+++ ifn.py
@@ -9,22 +9,20 @@ import numpy as np
 class inhibitory_feedback_network:
     def __init__(self, inhibition_type = 'divisive'):
         if inhibition_type in ['s', 'sub', 'subtract', 'subtractive']:
-            self.inhitype = 'subtractive'
+            self.inhitype = 'sub'
         else:
-            self.inhitype = 'divisive'
+            self.inhitype = 'div'
 
     def setup_matrix(self, M):
         self.M = np.asarray(M)
         self.num_Y, self.num_X = self.M.shape
 
     def recognise(self, X, epoch = 1000, rate = 0.1, epsilon = 0.001):
-        recog_methods = {'divisive': self.__recognise_div,
-                         'subtractive': self.__recognise_sub}
-        return recog_methods[self.inhitype](X, epoch, rate, epsilon)
-        # else:
-        #     return np.asarray([np.dot(self.M, X)])
+        Dxn_recog = {'div': self.__recognise_div,
+                     'sub': self.__recognise_sub}
+        return Dxn_recog[self.inhitype](X, epoch, rate, epsilon)
         
-    def __recognise_sub(self, X, epoch, rate, epsilon):
+    def _recognise_sub(self, X, epoch, rate, epsilon):
         Y = np.zeros(self.num_Y)
         outputs = np.empty((0, self.num_Y))
         for t in range(epoch):
@@ -35,7 +33,7 @@ class inhibitory_feedback_network:
             outputs = np.append(outputs, [Y], axis = 0)
         return outputs
     
-    def __recognise_div(self, X, epoch, rate, epsilon):
+    def _recognise_div(self, X, epoch, rate, epsilon):
         Y = np.zeros(self.num_Y)
         V = np.sum(self.M, axis = 1).astype(np.float)
         V[V == 0] = epsilon
@@ -49,12 +47,3 @@ class inhibitory_feedback_network:
             Y += dY * rate + noise
             outputs = np.append(outputs, [Y], axis = 0)
         return outputs
-
-# M = np.asarray([[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,1,1,0]])
-# fin = feedback_inhibition_network('d')
-# fin.setup_matrix(M)
-# print(fin.recognise([0,1,1,0])[-1])
-
-# bfin = feedback_inhibition_network('d')
-# bfin.setup_matrix(M.T)
-# print(bfin.recognise([0,1,1,1])[-1])
