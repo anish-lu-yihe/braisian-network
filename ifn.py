50d5c6fd5fba60e72cba7d547985a30847fbeaf7
L.Yihe <anish_alpha@hotmail.com>
2021-03-08 15:08:45 +0000

    refine inhibitory_feedback_network.recognise(): using ._dY_sub(), ._dY_div()

--- ifn.py
+++ ifn.py
@@ -8,43 +8,37 @@ import numpy as np
 
 class inhibitory_feedback_network:
     def __init__(self, M, inhibition_type = 'divisive'):
-        self.set_memory(M)
+        self.reset_memory(M)
         if inhibition_type in ['s', 'sub', 'subtract', 'subtractive']:
             self.inhitype = 'sub'
         else:
             self.inhitype = 'div'
 
-    def set_memory(self, M):
+    def reset_memory(self, M):
         self.M = np.asarray(M)
         self.num_Y, self.num_X = self.M.shape
+        
+    def _dY_sub(self, X, Y, epsilon):
+        E = X - np.dot(self.M.T, Y)
+        dY = np.dot(self.M, E)
+        return dY
+    
+    def _dY_div(self, X, Y, epsilon):
+        Xe = np.dot(self.M.T, Y).astype(np.float)
+        Xe[Xe == 0] = epsilon
+        E = np.divide(X, Xe)
+        dY = np.multiply(Y, np.divide(np.dot(self.M, E), self.V) - 1)
+        return dY
 
     def recognise(self, X, epoch = 1000, rate = 0.1, epsilon = 0.001):
-        Dxn_recog = {'div': self._recognise_div,
-                     'sub': self._recognise_sub}
-        return Dxn_recog[self.inhitype](X, epoch, rate, epsilon)
-        
-    def _recognise_sub(self, X, epoch, rate, epsilon):
+        _dY = {'div': self._dY_div, 'sub': self._dY_sub}[self.inhitype]        
+        self.V = np.sum(self.M, axis = 1).astype(np.float)
+        self.V[self.V == 0] = epsilon        
         Y = np.zeros(self.num_Y)
         outputs = [np.dot(self.M, X)]
         for t in range(epoch):
-            E = X - np.dot(self.M.T, Y)
-            dY = np.dot(self.M, E)
+            dY = _dY(X, Y, epsilon)
             noise = np.random.normal(scale = epsilon, size = self.num_Y)
-            Y += dY * rate + noise
-            outputs = np.append(outputs, [Y], axis = 0)
-        return outputs
-    
-    def _recognise_div(self, X, epoch, rate, epsilon):
-        Y = np.zeros(self.num_Y)
-        V = np.sum(self.M, axis = 1).astype(np.float)
-        V[V == 0] = epsilon
-        outputs = [np.dot(self.M, X)]
-        for t in range(epoch):
-            Xe = np.dot(self.M.T, Y).astype(np.float)
-            Xe[Xe == 0] = epsilon
-            E = np.divide(X, Xe)
-            dY = np.multiply(Y, np.divide(np.dot(self.M, E), V) - 1)
-            noise = np.abs(np.random.normal(scale = epsilon, size = self.num_Y))
             Y += dY * rate + noise
             outputs = np.append(outputs, [Y], axis = 0)
         return outputs
